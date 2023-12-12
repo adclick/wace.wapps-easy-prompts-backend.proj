9eb66e7436bade03d6229b997efb604bd11eedbf
nsaraiva <nuno.saraiva@wacestudio.com>
2023-12-12 12:32:20 +0000

    dev

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@ const app = express();
 app.use(express.json());
 
 // Use user routes
-app.use('/', userRoutes);
-app.use('/', filtersRoutes);
+app.use('/api', userRoutes);
+app.use('/api', filtersRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
